279dba60e9568ba9272b2d4d58930c2143f9b198
riadumitrescu <mariadumitrescuoffice@gmail.com>
2024-06-14 13:53:47 +0300

    social sharing

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const loader = document.getElementById('loader');
-    const content = document.getElementById('content');
-    const images = document.images;
-    let loadedImagesCount = 0;
-
-    function imageLoaded() {
-        loadedImagesCount++;
-        if (loadedImagesCount === images.length) {
-            loader.style.display = 'none';
-            content.style.display = 'block';
-        }
-    }
-
-    if (images.length === 0) {
-        // If there are no images, hide loader immediately
-        loader.style.display = 'none';
-        content.style.display = 'block';
-    } else {
-        for (let i = 0; i < images.length; i++) {
-            if (images[i].complete) {
-                imageLoaded();
-            } else {
-                images[i].addEventListener('load', imageLoaded);
-                images[i].addEventListener('error', imageLoaded);
-            }
-        }
-    }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    // When content is loaded, remove 'loading' class from body
+    document.body.classList.add("loaded");
+  });
+  
